8e8bb9e703967025579cb3f323a35f598eb1b1dc
PNdunguMaina <ndunguwamaina@gmail.com>
2022-12-14 23:45:03 +0300

    Create endpoint API

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      get '/messages/greetings/random', to: 'messages#random'
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
